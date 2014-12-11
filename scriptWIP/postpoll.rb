a3e1f2c8438f94c7d742bc9b110ff2318449aa0f
tyshipp <tyler.shipp@uky.edu>
2014-12-11 17:46:44 -0500

    Writes to the log file
    WIP Scripts now write to log file

--- scriptWIP/postpoll.rb
+++ scriptWIP/postpoll.rb
@@ -1,15 +1,20 @@
+#require string
 #Input UUID@Time@Key
 dataInIs = "12345@121120141632@AKey"
 
 #Parse input data
 uuid, *rest = dataInIs.split("@")
-stamp, key = rest.split("@")
-
+#stamp, key = rest.split("@")
+stamp = rest[0]
+key = rest[1]
+#stamp="$(echo "$@" | sed 's/ /\+/g')"
 
 #Record UUID
-open("proximitylog","ab") do |file|
-    file << stamp
-    file << uuid
+open("proximitylog.txt","ab") do |file|
+    file << "Date: " + stamp[0] + stamp[1] + "|" + stamp[2] + stamp[3] + "|" + s
+tamp[4] +stamp [5] + stamp[6] + stamp[7]
+    file << " Time: " + stamp[8] + stamp[9] + "|" + stamp[10] + stamp[11]
+    file << "\nUUID: " + uuid + "\n\n"
 end
 
 
